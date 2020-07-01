fcffb08ab8f2c63ef23e692ebbb1e0b35e6fa0b9
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-07-01 12:12:50 -0600

    Windows settings

---
+++ 01-first_window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+
+# Create a main window
+window = tk.Tk()
+# Call main loop
+window.mainloop()
---
+++ 02-window-settings.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+# Defining some constants
+TITLE = 'Tkinter Workshop'
+# Dimensions
+HEIGHT, WIDTH = 400, 400
+# Window position
+WINDOW_POS_X, WINDOW_POS_Y = 150, 150
+# Background color
+BG_COLOR = 'blue'
+
+
+# Create a main window
+window = tk.Tk()
+
+# Set title
+window.title(TITLE)
+# Set icon
+window.iconbitmap('src/icons/py.ico')
+# Set geometry
+# WIDTHxHEIGHT+X+Y
+window.geometry(f'{WIDTH}x{HEIGHT}+{WINDOW_POS_X}+{WINDOW_POS_Y}')
+# Set if it's resizable
+window.resizable(width=False, height=False)
+# Set background
+window.config(bg=BG_COLOR)
+
+
+# Call main loop
+window.mainloop()
